5713f931e10f291001c9f581f6ad20d16ab70ddd
Salman Saleem <37062860+yesmesalman@users.noreply.github.com>
2018-10-01 12:13:55 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+function getRealIpAddr()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))
+    {
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    {
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
